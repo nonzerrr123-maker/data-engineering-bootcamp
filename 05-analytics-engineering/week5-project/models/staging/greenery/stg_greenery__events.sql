acf8e19cdf51549a65e48c10119ef358d6664c98
Suntisuk promwang <nonzerrr123@gmail.com>
2026-09-06 16:15:14 +0700

    Match Day 5 event staging field name

--- 05-analytics-engineering/week5-project/models/staging/greenery/stg_greenery__events.sql
+++ 05-analytics-engineering/week5-project/models/staging/greenery/stg_greenery__events.sql
@@ -9,7 +9,7 @@ renamed_recasted as (
         session_id as session_guid,
         user_id as user_guid,
         page_url,
-        created_at as created_at_utc,
+        created_at as event_created_at_utc,
         event_type,
         order_id as order_guid,
         product_id as product_guid
