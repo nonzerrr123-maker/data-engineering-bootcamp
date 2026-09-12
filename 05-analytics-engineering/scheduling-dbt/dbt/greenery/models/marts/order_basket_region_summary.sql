aeeb40c54b501fc3ec27c2055da319905fda583c
Suntisuk promwang <nonzerrr123@gmail.com>
2026-09-12 15:15:32 +0700

    Remove extra basket-region challenge model

--- 05-analytics-engineering/scheduling-dbt/dbt/greenery/models/marts/order_basket_region_summary.sql
+++
@@ -1,8 +0,0 @@
-select
-    basket_size,
-    us_region,
-    count(distinct order_guid) as order_count,
-    sum(total_quantity) as item_quantity
-from {{ ref('int_orders_enriched') }}
-group by 1, 2
-order by 1, 2
